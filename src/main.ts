6f09ed5c899e2ac3e5021011bc391ebd5821c994
Komla Adzam <adzamkomla.dev@gmail.com>
2023-03-15 21:47:23 +0000

    Add validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
 import { ConfigService } from '@nestjs/config';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const port = config.get<number>('app.port');
   await app.listen(port);
