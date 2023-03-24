c27998e62d108a2841e637048e775aff92e66dbe
Komla Adzam <adzamkomla.dev@gmail.com>
2023-03-24 18:13:38 +0000

    update redis url

--- src/@common/configs/redis.config.ts
+++ src/@common/configs/redis.config.ts
@@ -3,4 +3,5 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('redis', () => ({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT || 6379,
+  url: process.env.REDIS_URL,
 }));
