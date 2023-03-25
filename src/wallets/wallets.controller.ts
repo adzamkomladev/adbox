3a8705b1dfc34d4cc0e8f4848da3ee7649b5a1cc
Komla Adzam <adzamkomla.dev@gmail.com>
2023-03-25 14:05:52 +0000

    fix wallet repo injection issues and add create wallet method

--- src/wallets/wallets.controller.ts
+++ src/wallets/wallets.controller.ts
@@ -1,9 +1,7 @@
 import {
   Controller,
   Get,
-  Post,
-  Body,
-  Patch,
+
   Param,
   Delete,
   HttpException,
@@ -14,20 +12,12 @@ import { ApiBadRequestResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { User } from '../auth/decorators/user.decorator';
 import { Auth } from '../auth/decorators/auth.decorator';
 
-import { CreateWalletDto } from './dto/create-wallet.dto';
-import { UpdateWalletDto } from './dto/update-wallet.dto';
-
 import { WalletsService } from './wallets.service';
 
 @ApiTags('wallets')
 @Controller('wallets')
 export class WalletsController {
   constructor(private readonly walletsService: WalletsService) {}
-
-  @Post()
-  create(@Body() createWalletDto: CreateWalletDto) {
-    return this.walletsService.create(createWalletDto);
-  }
 
   @Get()
   findAll() {
@@ -37,11 +27,6 @@ export class WalletsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.walletsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWalletDto: UpdateWalletDto) {
-    return this.walletsService.update(+id, updateWalletDto);
   }
 
   @Delete(':id')
