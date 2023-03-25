82e988561b6a7eeb94a0978d2a5f97aa2b5cc24e
Komla Adzam <adzamkomla.dev@gmail.com>
2023-03-25 13:43:15 +0000

    add auth module

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -8,9 +8,10 @@ import { WalletTransaction } from './entities/wallet-transaction.entity';
 import { WalletsService } from './wallets.service';
 
 import { WalletsController } from './wallets.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Wallet, WalletTransaction])],
+  imports: [MikroOrmModule.forFeature([Wallet, WalletTransaction]), AuthModule],
   controllers: [WalletsController],
   providers: [WalletsService],
 })
