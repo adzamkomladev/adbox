912289c72fe38db184331857fd07d769392f6a96
Komla Adzam <adzamkomla.dev@gmail.com>
2024-04-19 02:40:35 +0000

    feat: add new seed data

---
+++ src/@common/seeders/UserSeeder.ts
@@ -0,0 +1,39 @@
+import type { EntityManager } from '@mikro-orm/core';
+
+import { faker } from '@faker-js/faker';
+import { Seeder } from '@mikro-orm/seeder';
+
+import { Sex } from '../../users/enums/sex.enum';
+import { Status } from '../enums/status.enum';
+
+import { Role } from '../../users/entities/role.entity';
+import { User } from '../../users/entities/user.entity';
+
+export class UserSeeder extends Seeder {
+
+    async run(em: EntityManager): Promise<void> {
+        const roles = await em.findAll(Role, {
+            where: {
+                code: { $in: ['ADMIN', 'SUPER_ADMIN'] }
+            }
+        });
+
+        for (let i = 0; i < 20; i++) {
+            const chosenRole = faker.helpers.arrayElement(roles);
+
+            em.create(User, {
+                email: faker.internet.email(),
+                firstName: faker.person.firstName(),
+                lastName: faker.person.lastName(),
+                sex: faker.helpers.arrayElement([Sex.MALE, Sex.FEMALE]),
+                dateOfBirth: faker.date.birthdate(),
+                avatar: faker.image.avatar(),
+                role: chosenRole,
+                roleTitle: chosenRole.name,
+                status: Status.ACTIVE,
+                password: 'Abcde12345!',
+            });
+        }
+    }
+
+}
